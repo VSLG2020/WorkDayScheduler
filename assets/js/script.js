f118374c9f8fbd5855119ce3cf68842f25962111
Vanessa G <vlopez_jv@hotmail.com>
2020-11-17 19:35:39 -0800

    css modification

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,10 @@
+//curent hour
 var id="current_date", dt= new Date();
 var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
 document.write(time);
-
+//current date
 document.querySelector(".todays-date").innerText=moment().format("MM/DD/YYYY")
 
   console.log(moment().format("MM/DD/YYYY"));
 
 
-
-  
